34a6660cf676961902470422f6001e25cef8b7e5
Brad <Bradrockk@gmail.com>
2024-05-16 16:37:03 -0400

    fleshed out test command logging

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# To be filled soon
+This bot was made for testing and learning purposes on my end. 
+
+At the moment it doesn't do much but i plan to expand it in the future.
--- commands/utility/test.js
+++ commands/utility/test.js
@@ -1,11 +1,15 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const date = require('date-and-time');
 const now = new Date();
+const fs = require("fs");
+const path = require('path');
+const logpath = "../logs";
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('test')
-		.setDescription('Replies with time of response and logs to server.'),
+		.setDescription('Test scenario that logs time and basic things.'),
 	async execute(interaction) {
 		const PermCheck = PermissionsBitField.Flags.ADMINISTRATOR; 
         const memberPermissions = interaction.member.permissions;
@@ -17,6 +21,26 @@ module.exports = {
 		const reply = date.format(now, 'MM/DD/YYYY h:mm A [EST]'); 
 		const username = interaction.user.globalName
 		const server = interaction.guild.name
+	if (!fs.existsSync(logpath)) {
+    fs.mkdirSync(logpath, { recursive: true }); 
+	}
+
+	const info = {
+    "log": [
+        { "Server": `${server}` },
+        { "initiating user": `${username}` },
+        { "time sent back": `${reply}` }
+    ]
+	};
+	try {
+		time = date.format(now, 'MM_DD_YYYY h-mm-ss')
+		const filePath = path.join(logpath, `${time} log.json`);
+    	fs.writeFileSync(filePath, JSON.stringify(info, null, 2));
+    	console.log('File written successfully.', filePath);
+	} catch (error) {
+    	console.error(error);
+	}
+
 		await interaction.reply(`Recieved at ${reply} from ${username} in ${server}. Additional Logging information dumped in logs folder.`);
 	},
 };
